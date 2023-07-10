e35b0d8fbd60388927a9849c62ae2e014b20729f
dbabichev <dbabichev@phoenixit.ru>
2023-07-10 11:15:01 +0300

    word

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { updateSampleSection } from './sample-base';
+import { DocumentEditorContainerComponent, Toolbar } from '@syncfusion/ej2-react-documenteditor';
+import { TitleBar } from './title-bar';
+import {useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+DocumentEditorContainerComponent.Inject(Toolbar);
+// tslint:disable:max-line-length
+function Default() {
+  useEffect(() => {
+    updateSampleSection();
+    rendereComplete();
+  }, []);
+  let hostUrl = 'https://services.syncfusion.com/react/production/api/documenteditor/';
+  let container;
+  let titleBar;
+  function rendereComplete() {
+    window.onbeforeunload = function () {
+      return 'Want to save your changes?';
+    };
+    container.documentEditor.pageOutline = '#E0E0E0';
+    container.documentEditor.acceptTab = true;
+    container.documentEditor.resize();
+    titleBar = new TitleBar(document.getElementById('documenteditor_titlebar'), container.documentEditor, true);
+  }
+  return (<div className='control-pane'>
+    <div className='control-section'>
+      <div id='documenteditor_titlebar' className="e-de-ctn-title"></div>
+      <div id="documenteditor_container_body">
+        <DocumentEditorContainerComponent id="container" ref={(scope) => { container = scope; }} style={{ 'display': 'block' }} height={'790px'} serviceUrl={hostUrl} enableToolbar={true} locale='en-US'/>
+      </div>
     </div>
-  );
+  </div>);
 }
-
-export default App;
+export default Default;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NGaF5cXmdCdkx/WmFZfV1gd19DZlZVRWYuP1ZhSXxQdkJiWX9ddXJRRGZdVUI=')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
